e6b29f55099aab5f6c67459d50bb05e6259fd48f
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2018-09-09 20:08:17 +0200

    Allow extending untranspiled classes

--- packages/babel-helpers/src/helpers.js
+++ packages/babel-helpers/src/helpers.js
@@ -451,10 +451,8 @@ helpers.setPrototypeOf = helper("7.0.0-beta.0")`
   }
 `;
 
-helpers.construct = helper("7.0.0-beta.0")`
-  import setPrototypeOf from "setPrototypeOf";
-
-  function isNativeReflectConstruct() {
+helpers.isNativeReflectConstruct = helper("7.0.1")`
+  export default function _isNativeReflectConstruct() {
     if (typeof Reflect === "undefined" || !Reflect.construct) return false;
 
     // core-js@3
@@ -478,6 +476,11 @@ helpers.construct = helper("7.0.0-beta.0")`
       return false;
     }
   }
+`;
+
+helpers.construct = helper("7.0.0-beta.0")`
+  import setPrototypeOf from "setPrototypeOf";
+  import isNativeReflectConstruct from "isNativeReflectConstruct";
 
   export default function _construct(Parent, args, Class) {
     if (isNativeReflectConstruct()) {
@@ -661,6 +664,29 @@ helpers.possibleConstructorReturn = helper("7.0.0-beta.0")`
     return assertThisInitialized(self);
   }
 `;
+
+helpers.createSuper = helper("7.0.1")`
+  import getPrototypeOf from "getPrototypeOf";
+  import isNativeReflectConstruct from "isNativeReflectConstruct";
+  import possibleConstructorReturn from "possibleConstructorReturn";
+
+  export default function _createSuper(Derived) {
+    return function () {
+      var Super = getPrototypeOf(Derived), result;
+      if (isNativeReflectConstruct()) {
+        for (var l = arguments.length, args = new Array(l), i = 0; i < l; i++) {
+          args[i] = arguments[i];
+        }
+        // NOTE: This doesn't work if this.__proto__.constructor has been modified.
+        var NewTarget = getPrototypeOf(this).constructor;
+        result = Reflect.construct(Super, args, NewTarget);
+      } else {
+        result = Super.apply(this, arguments);
+      }
+      return possibleConstructorReturn(this, result);
+    }
+  }
+ `;
 
 helpers.superPropBase = helper("7.0.0-beta.0")`
   import getPrototypeOf from "getPrototypeOf";
