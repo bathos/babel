623cad587db666858a63fbce6d2347561fe49adb
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2019-01-13 01:16:23 +0100

    Add placeholders for class, function and try/catch

--- packages/babel-parser/src/parser/statement.js
+++ packages/babel-parser/src/parser/statement.js
@@ -1000,7 +1000,7 @@ export default class StatementParser extends ExpressionParser {
     this.state.strict = true;
 
     this.parseClassSuper(node);
-    this.parseClassBody(node);
+    node.body = this.parseClassBody();
 
     this.state.strict = oldStrict;
 
@@ -1027,7 +1027,7 @@ export default class StatementParser extends ExpressionParser {
     );
   }
 
-  parseClassBody(node: N.Class): void {
+  parseClassBody(): void {
     this.state.classLevel++;
 
     const state = { hadConstructor: false };
@@ -1089,9 +1089,9 @@ export default class StatementParser extends ExpressionParser {
       );
     }
 
-    node.body = this.finishNode(classBody, "ClassBody");
-
     this.state.classLevel--;
+
+    return this.finishNode(classBody, "ClassBody");
   }
 
   parseClassMember(
